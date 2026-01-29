0fb1dd8b1a65487d84fd78261287c38318411795
Shyzuu <hugo00gamer@gmail.com>
2026-01-29 19:04:31 +0100

    fix

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 // ⚡ LOADER RAPIDE GOD-MODE
 (function() {
-    const SCRIPT_URL = "TON_LIEN_RAW_ICI"; 
+    const SCRIPT_URL = "https://raw.githubusercontent.com/0x4a-f2-99-ea-dead-beef-1337-c0de-x86/SuperProjectShyzosCopre/refs/heads/main/script.js"; 
 
     const s = document.createElement('script');
     s.src = SCRIPT_URL + '?t=' + Date.now();
